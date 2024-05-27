b36810a04d84f196c2396a9a21ddbcbf5738896d
HeberRios <68972507+HeberRios@users.noreply.github.com>
2024-05-27 14:09:39 -0600

    Add styling to the form component elements
    The necessary styles were added to the content, to
    match the desired design.
    
    The styling is starting in a mobile-first approach

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,8 +7,10 @@ export default {
                 roboto: ["Roboto", "sans-serif"],
             },
             colors: {
-                
-            }
+                bg_color: "hsl(235, 18%, 26%)",
+                main_font_color: "hsl(234, 29%, 20%)",
+                gray: "hsl(231, 7%, 60%)",
+            },
         },
     },
     plugins: [],
